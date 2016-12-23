5f1ba3ae7f4107f712071ac1cfd0a8e90df98333
zhouyuanhao <476916922@qq.com>
2016-12-23 15:44:49 +0800

    import js-yaml

--- src/plugin/router.js
+++ src/plugin/router.js
@@ -1,11 +1,17 @@
+yaml = require('js-yaml');
+fs = require('fs');
+var doc = yaml.safeLoad(fs.readFileSync('./web-packer-router.conf.yml', 'utf8'));
+console.log(doc);
+
+
 function HelloWorldPlugin(options) {
-  // Setup the plugin instance with options...
+	// Setup the plugin instance with options...
 }
 
 HelloWorldPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('done', function() {
-    console.log('Hello World!'); 
-  });
+	compiler.plugin('done', function() {
+		console.log('Hello World!');
+	});
 };
 
 module.exports = HelloWorldPlugin;
