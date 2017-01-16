d345162c9997a81b29304fa557cfca8b383495c8
zhouyuanhao <476916922@qq.com>
2017-01-16 22:46:28 +0800

    add yaml

--- src/plugin/router.js
+++ src/plugin/router.js
@@ -1,6 +1,11 @@
 yaml = require('js-yaml');
 fs = require('fs');
-var doc = yaml.safeLoad(fs.readFileSync('./web-packer-router.conf.yml', 'utf8'));
+
+try {
+	var doc = yaml.safeLoad(fs.readFileSync('./web-packer-router.conf.yml', 'utf8'));
+} catch (e) {
+	var doc = yaml.safeLoad(fs.readFileSync('./src/plugin/web-packer-router.conf.yml', 'utf8'));
+}
 console.log(doc);
 
 
@@ -11,6 +16,20 @@ function HelloWorldPlugin(options) {
 HelloWorldPlugin.prototype.apply = function(compiler) {
 	compiler.plugin('done', function() {
 		console.log('Hello World!');
+	});
+
+	compiler.plugin('compilation', function(compilation, params) {
+		compilation.plugin('after-optimize-chunk-assets', function(chunks) {
+			console.log(chunks.map(function(c) {
+				return {
+					id: c.id,
+					name: c.name,
+					includes: c.modules.map(function(m) {
+						return m.request;
+					})
+				};
+			}));
+		});
 	});
 };
 
