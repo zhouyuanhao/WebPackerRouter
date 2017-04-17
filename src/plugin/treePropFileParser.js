2a834940ee77c992611fc917c2b6c90b31f14f46
zhouyuanhao <476916922@qq.com>
2017-04-17 22:19:38 +0800

    add space at start/end

--- src/plugin/treePropFileParser.js
+++ src/plugin/treePropFileParser.js
@@ -4,7 +4,23 @@ const treePropFileParser = {
 		var contentText = fs.readFileSync('envs.prop', 'utf-8');
 		console.log(contentText);
 
-		//parse tree file
+		//parse tree file here
+		var root = {
+			env: "",
+			parent = null;
+		}
+		var lines = contentText.split('\r\n');
+		var i, iReal;
+		iReal = 0;
+		for (int i = 0 i < lines.length; i++) {
+			//add leading space and appending space for search
+			if (lines.trim() == "") {
+				continue;
+			}
+			lines[iReal] = " " + lines[i] + " ";
+			iReal++;
+		}
+
 
 		global.routerPlugin.targetEnvChain = ['test', 'test2', 'test3'];
 		console.log('extend path:');
